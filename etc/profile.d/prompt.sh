f5bf23e7cce21d791ed377ebf1e4257f99603ca4
alexander <alexander.glass@gmail.com>
2016-01-18 00:44:59 -0500

    Support window title truncation

--- etc/profile.d/prompt.sh
+++ etc/profile.d/prompt.sh
@@ -11,10 +11,23 @@ Blue="\[\e[0;34m\]"
 Cyan="\[\e[0;36m\]"
 
 #PS1="$Cyan\u$Color_Off $Blue\w$Color_Off \$ "
-source /usr/local/bin/git-prompt.sh
+
+__set_window_title() {
+	if [ "$TERM" == "screen-256color" -a -n "$TMUX" ]; then
+		# if in a tmux session, set the window title with truncated username
+		printf "\ek${USER:0:1}@$HOSTNAME $PWD\e\\"
+	else
+		# update the terminal window title
+		echo -e "\e]2;${USER}@${HOSTNAME} : $PWD\a"
+	fi
+}
 
 # Display a git prompt that includes branch name
+source /usr/local/bin/git-prompt.sh
 PROMPT_COMMAND='__git_ps1 "$Cyan\u$Color_Off $Blue\w$Color_Off" "\\\$ "'
 GIT_PS1_DESCRIBE_STYLE='describe'
 GIT_PS1_SHOWDIRTYSTATE=1
 GIT_PS1_SHOWCOLORHINTS=1
+
+# Set tmux window title as u@hostname, trim strings to keep 17 char max length
+PROMPT_COMMAND+='; __set_window_title '
